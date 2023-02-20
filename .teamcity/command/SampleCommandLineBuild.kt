00dfaabb2e843f4f0152758f7be34face489a37a
AndreyKoltsov1997 <a99920641@gmail.com>
2023-02-20 17:58:20 +0100

    Add SampleCommandLineBuild configuration

---
+++ .teamcity/command/SampleCommandLineBuild.kt
@@ -0,0 +1,30 @@
+package command
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnMetricChange
+
+
+object SampleCommandLineBuild : BuildType({
+    name = "Sample Command Line Build"
+    steps {
+        script {
+            name = "Command Line Script"
+            scriptContent =
+                "echo \"##teamcity[buildStatisticValue key='myReportedCustomStatisticValue' value='10']\""
+        }
+    }
+
+    failureConditions {
+        failOnMetricChange {
+            param("metricKey", "myReportedCustomStatisticValue")
+            threshold = 2
+            units = BuildFailureOnMetric.MetricUnit.PERCENTS
+            comparison = BuildFailureOnMetric.MetricComparison.MORE
+            compareTo = build {
+                buildRule = lastSuccessful()
+            }
+        }
+    }
+})
