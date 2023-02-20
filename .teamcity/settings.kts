8bfdc4cc0fb42d3d08083c7d046d994c904ed993
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2023-02-20 14:19:31 +0100

    Add report of custom build metric.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-version = "2022.04"
-
 project {
   buildType(SampleCommandLineBuild)
 }
@@ -11,7 +9,18 @@ object SampleCommandLineBuild: BuildType({
     name = "Sample Command Line Build"
     steps {
         script {
-            scriptContent = "echo 'Hello world!'"
+            scriptContent = "echo \"##teamcity[buildStatisticValue key='myReportedCustomStatisticValue' value='10']\""
+        }
+    }
+    failureConditions {
+        failOnMetricChange {
+            param("metricKey", "myReportedCustomStatisticValue")
+            threshold = 2
+            units = BuildFailureOnMetric.MetricUnit.DEFAULT_UNIT
+            comparison = BuildFailureOnMetric.MetricComparison.MORE
+            compareTo = build {
+                buildRule = lastSuccessful()
+            }
         }
     }
 })
