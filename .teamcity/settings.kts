6405ae3e0ff6cb6236ac6f58f4e24da4b64ecc08
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2023-02-20 14:25:39 +0100

    Add excplicit 'Project' type

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,14 +1,17 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-project {
-  buildType(SampleCommandLineBuild)
+object Project : Project({
+    name = "Playground project"
+    description = "A project without any specific purpose - it's being used as a playground for various DSL features."
+    buildType(SampleCommandLineBuild)
 }
 
 object SampleCommandLineBuild: BuildType({
     name = "Sample Command Line Build"
     steps {
         script {
+            name = "Command Line Script"
             scriptContent = "echo \"##teamcity[buildStatisticValue key='myReportedCustomStatisticValue' value='10']\""
         }
     }
