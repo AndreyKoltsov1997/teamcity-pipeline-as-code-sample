d45f24b20af22218cc1da3c03baa5287fd6c8497
AndreyKoltsov1997 <a99920641@gmail.com>
2023-02-20 18:48:48 +0100

    Update value

--- .teamcity/command/SampleCommandLineBuild.kt
+++ .teamcity/command/SampleCommandLineBuild.kt
@@ -12,7 +12,7 @@ object SampleCommandLineBuild : BuildType({
         script {
             name = "Command Line Script"
             scriptContent =
-                "echo \"##teamcity[buildStatisticValue key='myReportedCustomStatisticValue' value='0']\""
+                "echo \"##teamcity[buildStatisticValue key='myReportedCustomStatisticValue' value='10']\""
         }
     }
 
