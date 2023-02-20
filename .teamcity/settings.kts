21bf77ea6ae5f57b7cb2ee52dc0c73a294fdc116
AndreyKoltsov1997 <a99920641@gmail.com>
2023-02-20 15:07:56 +0100

    Refactor import.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,19 +1,14 @@
-import jetbrains.buildServer.configs.kotlin.Project
-
 import command.*
-
-import jetbrains.buildServer.configs.kotlin.BuildType
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetric
-import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
-import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 
-object Project : Project({
+object PlaygroundProject : Project ({
     name = "Playground project"
     description = "A project without any specific purpose - it's being used as a playground for various DSL features."
     buildType(SampleCommandLineBuild)
 })
 
 version = "2022.04"
-project(Project)
+project(PlaygroundProject)
